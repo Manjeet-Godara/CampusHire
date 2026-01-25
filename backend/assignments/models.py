6295f3dfbd90ad18119c13b488c39745f13aa202
Manjeet Godara <manjeetgodara88020@gmail.com>
2026-01-25 20:27:36 +0530

    Submission Files can now be uploaded

--- backend/assignments/models.py
+++ backend/assignments/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from orgs.models import Company, Student
+import os
+import uuid
+
+def unique_file_name(instance,filename):
+    print(instance,",",filename)
+    ext=filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('uploads', filename)
 
 class Assignment(models.Model):
 
@@ -17,10 +25,10 @@ class AssignmentsSubmitted(models.Model):
     assignment=models.ForeignKey(Assignment,on_delete=models.CASCADE,default='')
     submitted_at=models.DateTimeField(default=timezone.now)
     is_late=models.BooleanField(default='')
-    similarity=models.DecimalField(max_digits=5,decimal_places=2)
+    similarity=models.DecimalField(max_digits=5,decimal_places=2,default=0)
     similarity_with=models.ForeignKey(Student,on_delete=models.CASCADE,default='',related_name='similarity_link')
     redflagged=models.BooleanField(default='')
-    #uploaded_file=models.FileField()
+    uploaded_file=models.FileField(upload_to=unique_file_name,default='',null=False,blank=False) 
 
 
     
