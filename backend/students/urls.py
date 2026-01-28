c5f362bc70db2fbe326c121544e5d6352c44f058
Manjeet Godara <manjeetgodara88020@gmail.com>
2026-01-29 00:20:05 +0530

    Completed student assignments page

--- backend/students/urls.py
+++ backend/students/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard',views.student_dashboard,name='student dashboard')
+    path('available_assignments',views.student_available_assignments,name='student_available_assignments'),
+    path('assignment/<int:id>/',views.assignment_submission,name='assignment_submission')
 ]
--- backend/students/views.py
+++ backend/students/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from assignments.models import Assignment
 
-def student_dashboard(request):
-    return render(request,'student_dashboard.html')
+def student_available_assignments(request):
+      
+    available_assigments=Assignment.objects.all()
+
+    return render(request,'student_available_assignments.html',{'assignments':available_assigments}) 
+
+def assignment_submission(request,id):
+    return render(request,'assignment_submission_page.html')
