ec8dc4c735d9e16f7795b20eeccf868c611b4f77
Manjeet Godara <manjeetgodara88020@gmail.com>
2026-01-29 00:51:28 +0530

    Remembering student through url tokens

--- backend/students/urls.py
+++ backend/students/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('available_assignments',views.student_available_assignments,name='student_available_assignments'),
-    path('assignment/<int:id>/',views.assignment_submission,name='assignment_submission')
+    path('available_assignments/<int:student_id>/',views.student_available_assignments,name='student_available_assignments'),
+    path('assignment/<int:student_id>/<int:assignment_id>/',views.assignment_submission,name='assignment_submission')
 ]
--- backend/students/views.py
+++ backend/students/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from assignments.models import Assignment
 
-def student_available_assignments(request):
+def student_available_assignments(request,student_id):
       
     available_assigments=Assignment.objects.all()
 
-    return render(request,'student_available_assignments.html',{'assignments':available_assigments}) 
+    return render(request,'student_available_assignments.html',{'assignments':available_assigments,'student_id':student_id}) 
 
-def assignment_submission(request,id):
+def assignment_submission(request,student_id,assignment_id):
+
     return render(request,'assignment_submission_page.html')
